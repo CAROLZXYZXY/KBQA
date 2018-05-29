fix bug: relation embedding gradient

baseline.py
@@ -2,12 +2,12 @@ import sys
 import time
 import argparse
 import math
-import random
+#import random
+from sklearn.utils import shuffle
 import numpy as np
 from tensorboardX import SummaryWriter
 
 import torch
-import torch.nn as nn
 from torch.autograd import Variable
 
 from data_preprocess import DataManager
@@ -50,7 +50,7 @@ def train(args):
         model = ABWIM(args.dropout, args.hidden_size, corpus.word_embedding, corpus.rela_embedding, q_len, r_len).cuda()
     elif args.model == 'HR-BiLSTM':
         model = HR_BiLSTM(args.dropout, args.hidden_size, corpus.word_embedding, corpus.rela_embedding).cuda()
-    print(model)
+    #print(model)
 
     if args.optimizer == 'Adadelta':
         print('optimizer: Adadelta')
@@ -86,7 +86,17 @@ def train(args):
                 nb_question += len(batch_data)
             elif args.batch_type == 'batch_obj':
                 question, pos_relas, pos_words, neg_relas, neg_words = zip(*batch_data)
+                q_len, pos_r_len, pos_w_len, neg_r_len, neg_w_len = zip(*train_data_len[batch_count-1])
+            '''
+            q_length = Variable(torch.LongTensor(q_len)).cuda()
+            pos_r_length = Variable(torch.LongTensor(pos_r_len)).cuda()
+            pos_w_length = Variable(torch.LongTensor(pos_w_len)).cuda()
+            neg_r_length = Variable(torch.LongTensor(neg_r_len)).cuda()
+            neg_w_length = Variable(torch.LongTensor(neg_w_len)).cuda()
+            '''
+                #print('len(q_len)', len(q_len)) # batch_size
             #print('len(question)', len(question))
+
             q = Variable(torch.LongTensor(question)).cuda()
             p_relas = Variable(torch.LongTensor(pos_relas)).cuda()
             p_words = Variable(torch.LongTensor(pos_words)).cuda()
@@ -98,6 +108,9 @@ def train(args):
             optimizer.zero_grad()
             all_pos_score = model(q, p_relas, p_words)
             all_neg_score = model(q, n_relas, n_words)
+#            all_pos_score = model(q, p_relas, p_words, q_length, pos_r_length, pos_w_length)
+#            sys.exit()
+#            all_neg_score = model(q, n_relas, n_words, q_length, neg_r_length, neg_w_length)
             model_end_time = time.time()
 
             loss = loss_function(all_pos_score, all_neg_score, ones)
@@ -181,19 +194,16 @@ def evaluation(model, mode='dev', global_step=None):
     if mode == 'test':
         input_data = test_data
         #print(model_test)
+        print(model_test)
     else:
         input_data = val_data
     nb_question = sum(len(batch_data) for batch_data in input_data)
-    #print('nb_question', nb_question)
-    
+    count = 0;
+    print('nb_question', nb_question)
     for batch_count, batch_data in enumerate(input_data, 1):
+        count+=1
         training_objs = [obj for q_obj in batch_data for obj in q_obj]
         question, pos_relas, pos_words, neg_relas, neg_words = zip(*training_objs)
-        #print(question[:5])
-        #print(pos_relas[:5])
-        #print(pos_words[:5])
-        #print(neg_relas[:5])
-        #print(neg_words[:5])
         q = Variable(torch.LongTensor(question)).cuda()
         p_relas = Variable(torch.LongTensor(pos_relas)).cuda()
         p_words = Variable(torch.LongTensor(pos_words)).cuda()
@@ -202,6 +212,7 @@ def evaluation(model, mode='dev', global_step=None):
         ones = Variable(torch.ones(len(question))).cuda()
         
         pos_score = model_test(q, p_relas, p_words)
+        #pos_alpha = model.ret_alpha(q, p_relas, p_words)
         neg_score = model_test(q, n_relas, n_words)
         loss = loss_function(pos_score, neg_score, ones)
         if torch.__version__ == '0.3.0.post4':
@@ -212,12 +223,15 @@ def evaluation(model, mode='dev', global_step=None):
 
         # Calculate accuracy and f1
         all_pos = pos_score.data.cpu().numpy()
+        #all_alpha = pos_alpha.data.cpu().numpy()
         all_neg = neg_score.data.cpu().numpy()
         start, end = 0, 0
         for idx, q_obj in enumerate(batch_data):
             end += len(q_obj)
             #print('start', start, 'end', end)
+            #input('Enter')
             score_list = [all_pos[start]]
+            #score_list_alpha = [all_alpha[start]]
             label_list = [1]
             batch_neg_score = all_neg[start:end]
             for ns in batch_neg_score:
@@ -225,17 +239,24 @@ def evaluation(model, mode='dev', global_step=None):
             label_list += [0] * len(batch_neg_score)
             start = end
             score_label = [(x, y) for x, y in zip(score_list, label_list)]
+            #alpha_label = [(x, y) for x, y in zip(score_list_alpha, label_list)]
+            #print("\n")
             #print(score_label[:10])
+            #print("\n")
             #print('len(score_list)', len(score_list), 'len(label_list)', len(label_list), 'len(score_label)', len(score_label))
             sorted_score_label = sorted(score_label, key=lambda x:x[0], reverse=True)
+            #sorted_alpha_label = sorted(alpha_label, key=lambda x:x[0], reverse=True)
+            #print("\n")
             #print(sorted_score_label)
+            #file = open("result.txt","w")
+            #file.write(sorted_alpha_label)
+            #file.close()
             total_acc += cal_acc(sorted_score_label)
             #print(total_acc)
-            #input('Enter')
-
 #        acc1 = total_acc / (batch_count * args.batch_size)
 #        acc2 = total_acc / question_counter
-
+   # print("\n")
+    #print(count)
     if mode == 'dev':
         writer.add_scalar('val_loss', average_loss.item(), global_step)
 
@@ -252,7 +273,7 @@ if __name__ == '__main__':
     # Set random seed
     torch.manual_seed(1234)
     torch.cuda.manual_seed(1234)
-    random.seed(1234)
+    #random.seed(1234)
     np.random.seed(1234)
 
     parser = argparse.ArgumentParser()
@@ -272,24 +293,34 @@ if __name__ == '__main__':
     parser.add_argument('--earlystop_tolerance', type=int, default=5)
     parser.add_argument('--save_model_path', type=str, default='')
     parser.add_argument('--pretrain_model', type=str, default=None)
+    parser.add_argument('--data_type', type=str, default='SQ') # [SQ/WQ]
     args = parser.parse_args()
     if args.model == 'ABWIM':
         args.margin = 0.1
         args.optimizer = 'Adadelta'
-    loss_function = nn.MarginRankingLoss(margin=args.margin)
+    loss_function = torch.nn.MarginRankingLoss(margin=args.margin)
 
     # Load data
-    corpus = DataManager()
+    corpus = DataManager(args.data_type)
     if args.train:
-        # shuffle training data
-        random.shuffle(corpus.token_train_data)
-        # split training data to train and validation
-        split_num = int(0.9*len(corpus.token_train_data))
-        print('split_num=', split_num)
-        train_data = corpus.token_train_data[:split_num]
-        val_data = corpus.token_train_data[split_num:]
-        print('training data length:', len(train_data))
-        print('validation data length:', len(val_data))
+        if args.data_type == 'SQ':
+            train_data = corpus.token_train_data
+            train_data_len = corpus.train_data_len
+            val_data = corpus.token_val_data
+            print('training data length:', len(train_data))
+            print('validation data length:', len(val_data))
+        else:
+            # shuffle training data
+            #random.shuffle(corpus.token_train_data)
+            shuffle(corpus.token_train_data, corpus.train_data_len, random_state=1234)
+            # split training data to train and validation
+            split_num = int(0.9*len(corpus.token_train_data))
+            print('split_num=', split_num)
+            train_data = corpus.token_train_data[:split_num]
+            train_data_len = corpus.train_data_len[:split_num]
+            val_data = corpus.token_train_data[split_num:]
+            print('training data length:', len(train_data))
+            print('validation data length:', len(val_data))
 
         if args.batch_type == 'batch_question':
             # batchify questions, uncomment Line 119, 120
@@ -297,9 +328,13 @@ if __name__ == '__main__':
         elif args.batch_type == 'batch_obj':
             # batchify train_objs, uncomment Line 121
             flat_train_data = [obj for q_obj in train_data for obj in q_obj]
+            flat_train_data_len = [obj for q_obj in train_data_len for obj in q_obj]
             print('len(flat_train_data)', len(flat_train_data))
-            random.shuffle(flat_train_data)
+            print('len(flat_train_data_len)', len(flat_train_data_len))
+            #random.shuffle(flat_train_data)
+            shuffle(flat_train_data, flat_train_data_len, random_state=1234)
             train_data = batchify(flat_train_data, args.batch_obj_size)
+            train_data_len = batchify(flat_train_data_len, args.batch_obj_size)
         val_data = batchify(val_data, args.batch_question_size)
 
         # Create SummaryWriter
@@ -327,5 +362,5 @@ if __name__ == '__main__':
                 outfile.write(str(test_acc)+'\t'+args.pretrain_model+'\n')
 
     # Close writer
-    writer.close()
+    #writer.close()
 

data_preprocess.py
@@ -13,55 +13,87 @@ import torch
 from torch.autograd import Variable
 import math
 class DataManager:
-    def __init__(self):
-        self.train_data_path = 'KBQA_RE_data/webqsp_relations/WebQSP.RE.train.with_boundary.withpool.dlnlp.txt'
-        self.test_data_path = 'KBQA_RE_data/webqsp_relations/WebQSP.RE.test.with_boundary.withpool.dlnlp.txt'
+    def __init__(self, data_type):
+        self.data_type = data_type
+        if self.data_type == 'SQ':
+            self.train_data_path = 'KBQA_RE_data/sq_relations/train.replace_ne.withpool'
+            self.val_data_path = 'KBQA_RE_data/sq_relations/valid.replace_ne.withpool'
+            self.test_data_path = 'KBQA_RE_data/sq_relations/test.replace_ne.withpool'
+            self.word_embedding_path = 'SQ_word_emb_300d.txt'
+            self.rela_embedding_path = 'SQ_rela_emb_300d.txt'
+            self.relations_map = self.load_relations_map('KBQA_RE_data/sq_relations/relation.2M.list')
+        else:
+            self.train_data_path = 'KBQA_RE_data/webqsp_relations/WebQSP.RE.train.with_boundary.withpool.dlnlp.txt'
+            self.test_data_path = 'KBQA_RE_data/webqsp_relations/WebQSP.RE.test.with_boundary.withpool.dlnlp.txt'
+            self.word_embedding_path = 'KBQA_RE_word_emb_300d.txt'
+            self.rela_embedding_path = 'KBQA_RE_rela_emb_300d.txt'
+            self.relations_map = self.load_relations_map('KBQA_RE_data/webqsp_relations/relations.txt')
 #        self.train_data_path = '1hop_2hop_data/WebQSP.1hop'
 #        self.test_data_path =  '1hop_2hop_data/WebQSP.2hop'
-        self.word_embedding_path = 'KBQA_RE_word_emb_300d.txt'
-        self.rela_embedding_path = 'KBQA_RE_rela_emb_300d.txt'
         #self.word_embedding_path = 'KBQA_RE_word_emb_300d_last.txt'
         #self.rela_embedding_path = 'KBQA_RE_rela_emb_300d_last.txt'
         self.emb_dim = 300
-        self.relations_map = {}
         self.word_dic = {}
         self.word_embedding = []
         self.rela_dic = {}
         self.rela_embedding = []
 
-        self.relations_map = self.load_relations_map('KBQA_RE_data/webqsp_relations/relations.txt')
         #print('Original training questions: 3116')
         #print('Original testing questions: 1649')
-        train_data = self.gen_train_data(self.train_data_path) 
-        test_data = self.gen_train_data(self.test_data_path) 
         print('Filter out questions without negative training samples.')
+        train_data, self.train_data_len = self.gen_train_data(self.train_data_path) 
         print(f'Train data length:{len(train_data)}')
+        test_data, self.test_data_len = self.gen_train_data(self.test_data_path) 
         print(f'Test data length:{len(test_data)}')
+        if self.data_type == 'SQ':
+            val_data, self.val_data_len = self.gen_train_data(self.val_data_path) 
+            print(f'Validation data length:{len(val_data)}')
 
         if not os.path.isfile(self.word_embedding_path):
             print(self.word_embedding_path, 'not exist!')
-            self.save_embeddings(train_data+test_data)
+            if self.data_type == 'SQ':
+                self.save_embeddings(train_data+val_data+test_data)
+            else:
+                self.save_embeddings(train_data+test_data) 
             print()
         self.word_dic, self.word_embedding = self.load_embeddings(self.word_embedding_path)
         self.rela_dic, self.rela_embedding = self.load_embeddings(self.rela_embedding_path)
         token_train_data = self.tokenize_train_data(train_data)
-        token_test_data = self.tokenize_train_data(test_data)
         print(f'Tokened train data length:{len(token_train_data)}')
+        token_test_data = self.tokenize_train_data(test_data)
         print(f'Tokened test data length:{len(token_test_data)}')
-        self.maxlen_q, maxlen_pos_r, maxlen_pos_w, maxlen_neg_r, maxlen_neg_w = self.find_maxlength(token_train_data+token_test_data)
+        if self.data_type == 'SQ':
+            token_val_data = self.tokenize_train_data(val_data)
+            print(f'Tokened val data length:{len(token_val_data)}')
+
+        if self.data_type == 'SQ':
+            self.q_seqlen, self.pos_r_seqlen, self.pos_w_seqlen, self.neg_r_seqlen, self.neg_w_seqlen, self.maxlen_q, maxlen_pos_r, maxlen_pos_w, maxlen_neg_r, maxlen_neg_w = self.find_maxlength(token_train_data+token_val_data+token_test_data)
+        else:
+            self.q_seqlen, self.pos_r_seqlen, self.pos_w_seqlen, self.neg_r_seqlen, self.neg_w_seqlen, self.maxlen_q, maxlen_pos_r, maxlen_pos_w, maxlen_neg_r, maxlen_neg_w = self.find_maxlength(token_train_data+token_test_data)
+#        train_question, train_pos_relas, train_pos_words, train_neg_relas, train_neg_words = zip(*token_train_data)
+#        test_question, test_pos_relas, test_pos_words, test_neg_relas, test_neg_words = zip(*token_test_data)
+#        self.seqlen_q, self.maxlen_q = self.find_maxlen(train_question + test_question)
+#        self.seqlen_pos_r, maxlen_pos_r = self.find_maxlen(train_pos_relas + test_pos_relas)
+#        self.seqlen_pos_w, maxlen_pos_w = self.find_maxlen(train_pos_words + test_pos_words)
+#        self.seqlen_neg_r, maxlen_neg_r = self.find_maxlen(train_neg_relas + test_neg_relas)
+#        self.seqlen_neg_w, maxlen_neg_w = self.find_maxlen(train_neg_words + test_neg_words)
         self.maxlen_r = max(maxlen_pos_r, maxlen_neg_r)
         self.maxlen_w = max(maxlen_pos_w, maxlen_neg_w)
         print(f'maxlen_q:{self.maxlen_q}, maxlen_r:{self.maxlen_r}, maxlen_w:{self.maxlen_w}')
+        print('len(q_seqlen)', len(self.q_seqlen))
         #print(f'maxlen_pos_r:{maxlen_pos_r}, maxlen_neg_r:{maxlen_neg_r}')
         #print(f'maxlen_pos_w:{maxlen_pos_w}, maxlen_neg_w:{maxlen_neg_w}')
+
         self.token_train_data = self.pad_train_data(token_train_data, self.maxlen_q, self.maxlen_r, self.maxlen_w, self.maxlen_r, self.maxlen_w)
+        if self.data_type == 'SQ':
+            self.token_val_data = self.pad_train_data(token_val_data, self.maxlen_q, self.maxlen_r, self.maxlen_w, self.maxlen_r, self.maxlen_w)
         self.token_test_data = self.pad_train_data(token_test_data, self.maxlen_q, self.maxlen_r, self.maxlen_w, self.maxlen_r, self.maxlen_w)
-#        self.check_input_data(test_data, self.token_test_data)
+        self.check_input_data(test_data, self.token_test_data)
 
     def check_input_data(self, origin_data, token_data):
         print()
         print('Check token result')
-        print('# objs in the 1st question of test data:', len(test_data[0]), len(self.token_test_data[0]))
+        print('# objs in the 1st question of test data:', len(origin_data[0]), len(token_data[0]))
         print('1st obj in the 1st question of test data:')
         print('obj: (question, pos_relas, pos_words, neg_relas, neg_words)')
         print(origin_data[0][0])
@@ -89,50 +121,73 @@ class DataManager:
         ''' Return training_data_list [[(question, pos_relas, pos_words, neg_relas, neg_words) * neg_size] * q_size]
         '''
         data_list = []
+        data_len_list = []
         #pos_gt_1_counter = 0
+        total_instance_counter = 0
         print('Load', path)
         #start = time.time()
         with open(path) as infile:
             for line in infile: 
                 q_list = []
+                seqlen_list = []
                 tokens = line.strip().split('\t')
                 pos_relations = tokens[0]
                 neg_relations = tokens[1]
-                question = tokens[2].replace('$ARG1','').replace('$ARG2','').strip().split(' ')
+                if self.data_type == 'SQ':
+                    question = tokens[2].strip().split(' ')
+                else:
+                    question = tokens[2].replace('$ARG1','').replace('$ARG2','').strip().split(' ')
                 #if len(pos_relations.split(' ')) != 1:
                 #    pos_gt_1_counter += 1
                 #for pos_id in pos_relations.split(' '):
                 pos_id = pos_relations.split(' ')[0]
-                pos_relas, pos_words = self.split_relation(pos_id)
+                total_instance_counter += 1
+                pos_rela, pos_word = self.split_relation(pos_id)
                 for neg_id in neg_relations.split(' '):
                     # skip blank relation (relation_id 1797 = '')
-                    if neg_id == '1797':
-                        continue
+                    if self.data_type == 'SQ':
+                        if neg_id == 'noNegativeAnswer':
+                            continue
+                    else:
+                        if neg_id == '1797':
+                            continue
                     neg_relas, neg_words = self.split_relation(neg_id)
-                    q_list.append((question, pos_relas, pos_words, neg_relas, neg_words))
+                    total_instance_counter += 1
+                    q_list.append((question, pos_rela, pos_word, neg_relas, neg_words))
+                    seqlen_list.append((len(question), len(pos_rela), len(pos_word), len(neg_relas), len(neg_words)))
                     #print(q_list)
                     #sys.exit()
                 if len(q_list) > 0:
                     data_list.append(q_list)
+                    data_len_list.append(seqlen_list)
         #print(f'Time elapsed:{time.time()-start:.2f}')
         #print('pos_gt_1_counter', pos_gt_1_counter)
-        return data_list
+        print('average instances per question', total_instance_counter/len(data_list))
+        return data_list, data_len_list
 
     def split_relation(self, relation_id):
         '''Return relation_token_list and relation_token_name_list
         '''
         rela_list = []
         word_list = []
+
         relation_names = self.relations_map[int(relation_id)]
-        for relation_name in relation_names.split('..'):
-            #last_name = relation_name.split('.')[-1]
-            #rela_list.append(last_name)
-            #for word in last_name.split('_'):
-            #    word_list.append(word)
-            for relation_token in relation_name.split('.'):
+
+        if self.data_type == 'SQ':
+            for relation_token in relation_names.strip('/').split('/'):
                 rela_list.append(relation_token)
                 for word in relation_token.split('_'):
                     word_list.append(word)
+        else:
+            for relation_name in relation_names.split('..'):
+                #last_name = relation_name.split('.')[-1]
+                #rela_list.append(last_name)
+                #for word in last_name.split('_'):
+                #    word_list.append(word)
+                for relation_token in relation_name.split('.'):
+                    rela_list.append(relation_token)
+                    for word in relation_token.split('_'):
+                        word_list.append(word)
         return rela_list, word_list
 
     def find_unique(self, data):
@@ -159,7 +214,8 @@ class DataManager:
     def load_word_embedding_from_gensim(self, input_path):
         print('Load pretrain word embedding from', input_path)
         #start = time.time()
-        model = gensim.models.Word2Vec.load_word2vec_format(input_path, binary=True)
+#        model = gensim.models.Word2Vec.load_word2vec_format(input_path, binary=True)
+        model = gensim.models.KeyedVectors.load_word2vec_format(input_path, binary=True)
         #print(f'Time elapsed:{time.time()-start:.2f}') 
         return model
 
@@ -244,7 +300,6 @@ class DataManager:
         return token_data
 
     def pad_train_data(self, data, maxlen_q, maxlen_pos_r, maxlen_pos_w, maxlen_neg_r, maxlen_neg_w):
-    #def pad_train_data(self, data):
         ''' Input: training_data_list [[(question, pos_relas, pos_words, neg_relas, neg_words) * neg_size] * q_size]
         '''
         padded_data = []
@@ -269,31 +324,50 @@ class DataManager:
         #else:
         #    return sentence[:max_len]
 
-    def find_maxlength(self, data):
-        maxlen_q, maxlen_pos_r, maxlen_pos_w, maxlen_neg_r, maxlen_neg_w = 0, 0, 0, 0, 0
+    def find_maxlen(self, data):
+        maxlen = 0
+        seq_len = []
         for q_data in data:
             for obj in q_data:
-                if len(obj[0]) > maxlen_q:
-                    maxlen_q = len(obj[0])
-                if len(obj[1]) > maxlen_pos_r:
-                    maxlen_pos_r = len(obj[1])
-                if len(obj[2]) > maxlen_pos_w:
-                    maxlen_pos_w = len(obj[2])
+                seq_len.append(len(obj))
+                if len(obj) > maxlen:
+                    maxlen = len(obj)
+        return seq_len, maxlen
+
+    def find_maxlength(self, data):
+        q_seqlen = []
+        pos_r_seqlen = []
+        pos_w_seqlen = []
+        neg_r_seqlen = []
+        neg_w_seqlen = []
+        maxlen_q, maxlen_pos_r, maxlen_pos_w, maxlen_neg_r, maxlen_neg_w = 0, 0, 0, 0, 0
+        for q_data in data:
+            if len(q_data[0][0]) > maxlen_q:
+                maxlen_q = len(q_data[0][0])
+            if len(q_data[0][1]) > maxlen_pos_r:
+                maxlen_pos_r = len(q_data[0][1])
+            if len(q_data[0][2]) > maxlen_pos_w:
+                maxlen_pos_w = len(q_data[0][2])
+            for obj in q_data:
+                q_seqlen.append(len(obj[0]))
+                pos_r_seqlen.append(len(obj[1]))
+                pos_w_seqlen.append(len(obj[2]))
+                neg_r_seqlen.append(len(obj[3]))
+                neg_w_seqlen.append(len(obj[4]))
                 if len(obj[3]) > maxlen_neg_r:
                     maxlen_neg_r = len(obj[3])
                 if len(obj[4]) > maxlen_neg_w:
                     maxlen_neg_w = len(obj[4])
-        return maxlen_q, maxlen_pos_r, maxlen_pos_w, maxlen_neg_r, maxlen_neg_w
+        return q_seqlen, pos_r_seqlen, pos_w_seqlen, neg_r_seqlen, neg_w_seqlen, maxlen_q, maxlen_pos_r, maxlen_pos_w, maxlen_neg_r, maxlen_neg_w
     
     def load_relations_map(self, path):
         ''' Return self.relations_map = {idx:relation_names}
         '''
         relations_map = {}
+        print('Load', path)
         with open(path) as infile:
-            idx = 1
-            for line in infile:
+            for idx, line in enumerate(infile, 1):
                 relations_map[idx] = line.strip()
-                idx += 1
         return relations_map
     
     def question_preprocess(self, path):

model.py
@@ -10,6 +10,9 @@ class HR_BiLSTM(nn.Module):
         self.hidden_size = hidden_size
         self.nb_layers = 1
         self.dropout_rate = dropout_rate
+        self.dropout = nn.Dropout(self.dropout_rate)
+        self.cos = nn.CosineSimilarity(1)
+        
         # Word Embedding layer
         self.word_embedding = nn.Embedding(word_emb.shape[0], word_emb.shape[1])
         self.word_embedding.weight = nn.Parameter(th.from_numpy(word_emb).float())
@@ -17,69 +20,112 @@ class HR_BiLSTM(nn.Module):
         # Rela Embedding layer
         self.rela_embedding = nn.Embedding(rela_emb.shape[0], rela_emb.shape[1])
         self.rela_embedding.weight = nn.Parameter(th.from_numpy(rela_emb).float())
-        self.rela_embedding.weight.requires_grad = False # fix the embedding matrix
+        self.rela_embedding.weight.requires_grad = True # fix the embedding matrix
         # LSTM layer
-        self.bilstm_1 = nn.LSTM(word_emb.shape[1], hidden_size, num_layers=self.nb_layers, bidirectional=True)
-        self.bilstm_2 = nn.LSTM(hidden_size*2, hidden_size, num_layers=self.nb_layers, bidirectional=True)
+        self.bilstm_1 = nn.LSTM(word_emb.shape[1], hidden_size, num_layers=self.nb_layers, bidirectional=True, batch_first=True)
+        self.bilstm_2 = nn.LSTM(hidden_size*2, hidden_size, num_layers=self.nb_layers, bidirectional=True, batch_first=True)
 
-        self.dropout = nn.Dropout(self.dropout_rate)
+    def revert_order(self, sorted_seq, sorted_seqidx):
+        ori_seq = sorted_seq.clone()
+        for new_idx, ori_idx in enumerate(sorted_seqidx):
+            ori_seq[ori_idx] = sorted_seq[new_idx]
+        return ori_seq
 
-        self.cos = nn.CosineSimilarity(1)
-        
+    def pack_seq(self, ori_seq, seqlen):
+        sorted_seqlen, sorted_seqidx = th.sort(seqlen, descending=True)
+        sorted_seq = ori_seq.clone()
+        for new_idx, ori_idx in enumerate(sorted_seqidx):
+            sorted_seq[new_idx] = ori_seq[ori_idx]
+        packed_seq  = nn.utils.rnn.pack_padded_sequence(sorted_seq, sorted_seqlen, batch_first=True)
+        return packed_seq, sorted_seqidx
+
     def forward(self, question, rela_relation, word_relation):
-        question = th.transpose(question, 0, 1)
-        rela_relation = th.transpose(rela_relation, 0, 1)
-        word_relation = th.transpose(word_relation, 0, 1)
+#    def forward(self, question, rela_relation, word_relation, q_seqlen, rela_seqlen, word_seqlen):
+        # Input shape is [batch_size, maxlen]
+        # Embedding shape is [batch_size, maxlen, emb_dim]
+        question = self.dropout(self.word_embedding(question))
+        rela_relation = self.dropout(self.rela_embedding(rela_relation))
+        word_relation = self.dropout(self.word_embedding(word_relation))
 
-        question = self.word_embedding(question)
-        #print('question_emb.shape', question.shape)
-        rela_relation = self.rela_embedding(rela_relation)
-        #print('rela_relation_emb.shape', rela_relation.shape)
-        word_relation = self.word_embedding(word_relation)
-        #print('word_relation_emb.shape', word_relation.shape)
-        #print()
-
-        question = self.dropout(question)
-        rela_relation = self.dropout(rela_relation)
-        word_relation = self.dropout(word_relation)
-
-#        self.bilstm_1.flatten_parameters()
+        # output shape of bilstm is [batch_size, maxlen, hidden_size*2]
         question_out_1, question_hidden = self.bilstm_1(question)
         question_out_1 = self.dropout(question_out_1)
-        #print('question_out_1.shape', question_out_1.shape)
-#        self.bilstm_2.flatten_parameters()
         question_out_2, _ = self.bilstm_2(question_out_1)
         question_out_2 = self.dropout(question_out_2)
-        #print('question_out_2.shape', question_out_2.shape)
+        '''pack_pad_sequence
+        packed_question, sorted_q_idx = self.pack_seq(question, q_seqlen)
+        question_out_1, question_hidden = self.bilstm_1(packed_question)
+        question_out_1, _ = th.nn.utils.rnn.pad_packed_sequence(question_out_1, batch_first=True)
+        question_out_1 = self.dropout(question_out_1)
+        packed_question_out_1, _ = self.pack_seq(question_out_1, q_seqlen)
+        question_out_2, _ = self.bilstm_2(packed_question_out_1)
+        question_out_2, _ = th.nn.utils.rnn.pad_packed_sequence(question_out_2, batch_first=True)
+        question_out_2 = self.dropout(question_out_2)
+        '''
         
         # 1st way of Hierarchical Residual Matching
         q12 = question_out_1 + question_out_2
-        q12 = q12.permute(1, 2, 0)
-        #print('q12.shape', q12.shape)
-        question_representation = nn.MaxPool1d(q12.shape[2])(q12) 
-        question_representation = question_representation.squeeze(2)
+        # Transform q12 shape from [batch_size, maxlen, hidden_size*2] to [batch_size, hidden_size*2, maxlen]
+        q12 = q12.permute(0, 2, 1)
+        # After maxpooling, the question representation shape = [batch_size, hidden_size*2]
+        question_representation = nn.MaxPool1d(q12.shape[2])(q12).squeeze()
+        print(question_representation.shape)
         # 2nd way of Hierarchical Residual Matching
         #q1_max = nn.MaxPool1d(question_out_1.shape[2])(question_out_1)
         #q2_max = nn.MaxPool1d(question_out_2.shape[2])(question_out_2)
         #question_representation = q1_max + q2_max
-        #print('question_representation.shape', question_representation.shape) 
 
-        #print()
-#        self.bilstm_1.flatten_parameters()
         word_relation_out, word_relation_hidden = self.bilstm_1(word_relation)
         word_relation_out = self.dropout(word_relation_out)
-        #print('word_relation_out.shape', word_relation_out.shape)
-#        self.bilstm_1.flatten_parameters()
+        #print(word_relation_out.shape)
         rela_relation_out, rela_relation_hidden = self.bilstm_1(rela_relation, word_relation_hidden)
         rela_relation_out = self.dropout(rela_relation_out)
-        #print('rela_relation_out.shape', rela_relation_out.shape)
-        r = th.cat([rela_relation_out, word_relation_out], 0)
-        r = r.permute(1, 2, 0)
+        #print(rela_relation_out.shape)
+        r = th.cat([rela_relation_out, word_relation_out], 1)
         #print('r.shape', r.shape)
-        relation_representation = nn.MaxPool1d(r.shape[2])(r)
-        relation_representation = relation_representation.squeeze(2)
-        #print('relation_representation.shape', relation_representation.shape)
+        r = r.permute(0, 2, 1)
+        #print('r.shape', r.shape)
+        relation_representation = nn.MaxPool1d(r.shape[2])(r).squeeze()
+        print(relation_representation.shape)
+        '''pack_pad_sequence
+        # Revert to original order
+        question_representation = self.revert_order(question_representation, sorted_q_idx)
 
+        packed_rela_relation, sorted_rela_idx = self.pack_seq(rela_relation, rela_seqlen)
+        packed_word_relation, sorted_word_idx = self.pack_seq(word_relation, word_seqlen)
+        
+        word_relation_out, word_relation_hidden = self.bilstm_1(packed_word_relation)
+        word_relation_out, _ = th.nn.utils.rnn.pad_packed_sequence(word_relation_out, batch_first=True)
+        word_relation_out = self.dropout(word_relation_out)
+        print('word_relation_out.shape', word_relation_out.shape)
+
+        # Revert to original order
+        print(len(word_relation_hidden))
+        print(len(word_relation_hidden[0]))
+        print(len(word_relation_hidden[0][0]))
+        print(len(word_relation_hidden[0][0][0]))
+        sys.exit()
+        word_relation_h = self.revert_order(word_relation_hidden[0], sorted_word_idx)
+        word_relation_c = self.revert_order(word_relation_hidden[1], sorted_word_idx)
+        # Transform to rela_relation order
+        word_relation_h = self.revert_order(word_relation_h, sorted_rela_idx)
+        word_relation_c = self.revert_order(word_relation_c, sorted_rela_idx)
+
+        rela_relation_out, rela_relation_hidden = self.bilstm_1(packed_rela_relation, (word_relation_h, word_relation_c))
+        rela_relation_out, _ = th.nn.utils.rnn.pad_packed_sequence(rela_relation_out, batch_first=True)
+        rela_relation_out = self.dropout(rela_relation_out)
+        print('rela_relation_out.shape', rela_relation_out.shape)
+        r = th.cat([rela_relation_out, word_relation_out], 0)
+        print('r.shape', r.shape)
+        r = r.permute(0, 2, 1)
+        print('r.shape', r.shape)
+        relation_representation = nn.MaxPool1d(r.shape[0])(r).squeeze()
+        print('relation_representation.shape', relation_representation.shape)
+        sys.exit()
+
+        # Revert to original order
+        relation_representation = self.revert_order(relation_representation, sorted_rela_idx)
+        '''
         score = self.cos(question_representation, relation_representation)
         #print('score.shape', score.shape)
         return score
@@ -105,7 +151,6 @@ class Model(nn.Module):
         self.cos = nn.CosineSimilarity(dim=1)
         self.tanh = nn.Tanh()
         return
-
     def forward(self, ques_x, rela_text_x, rela_x):
         ques_x = th.transpose(ques_x, 0, 1)
         rela_text_x = th.transpose(rela_text_x, 0, 1)
@@ -185,7 +230,32 @@ class ABWIM(nn.Module):
     def init_hidden(self, batch_size):
         return (Variable(th.zeros(2, batch_size, self.hidden_size)).cuda(),
                 Variable(th.zeros(2, batch_size, self.hidden_size)).cuda())
-        
+    def ret_alpha(self, question, rela_relation, word_relation):
+        question = th.transpose(question, 0, 1)
+        rela_relation = th.transpose(rela_relation, 0, 1)
+        word_relation = th.transpose(word_relation, 0, 1)
+        question = self.word_embedding(question)
+        question = self.dropout(question)
+        rela_relation = self.rela_embedding(rela_relation)
+        rela_relation = self.dropout(rela_relation)
+        word_relation = self.word_embedding(word_relation)
+        word_relation = self.dropout(word_relation)
+        #self.bilstm.flatten_parameters()
+        question_out, _ = self.bilstm(question)
+        question_out = question_out.permute(1,2,0)
+        question_out = self.dropout(question_out)
+        word_relation_out, word_relation_hidden = self.bilstm(word_relation)
+        rela_relation_out, _ = self.bilstm(rela_relation, word_relation_hidden)
+        word_relation_out = self.dropout(word_relation_out)
+        rela_relation_out = self.dropout(rela_relation_out)
+        relation = th.cat([rela_relation_out, word_relation_out], 0)
+        relation = relation.permute(1,0,2)
+
+        # attention layer
+        energy = th.matmul(relation, self.W)
+        energy = th.matmul(energy, question_out)
+        alpha = F.softmax(energy, dim=-1)
+        return alpha 
     def forward(self, question, rela_relation, word_relation):
         question = th.transpose(question, 0, 1)
         rela_relation = th.transpose(rela_relation, 0, 1)
@@ -210,6 +280,10 @@ class ABWIM(nn.Module):
         relation = relation.permute(1,0,2)
 
         # attention layer
+        #energy_tmp = energy.view(energy.shape[0], energy.shape[1]*energy.shape[2])
+        #alpha = F.softmax(energy_tmp, dim=-1)
+        #alpha = alpha.view(energy.shape[0], energy.shape[1], energy.shape[2])
+
         energy = th.matmul(relation, self.W)
         energy = th.matmul(energy, question_out)
         alpha = F.softmax(energy, dim=-1)
@@ -230,3 +304,50 @@ class ABWIM(nn.Module):
         score = self.linear(h).squeeze()
         return score
     
+if __name__ == '__main__':
+    import numpy as np
+    dropout = 0.35
+    hidden_size = 100
+    learning_rate = 1
+
+    word_embedding_path = 'SQ_word_emb_300d.txt'
+    rela_embedding_path = 'SQ_rela_emb_300d.txt'
+    word_embedding = []
+    rela_embedding = []
+    with open(word_embedding_path) as infile:
+        for line in infile:
+            tokens = line.strip().split()
+            word_embedding.append([float(x) for x in tokens[1:]]) 
+    with open(rela_embedding_path) as infile:
+        for line in infile:
+            tokens = line.strip().split()
+            rela_embedding.append([float(x) for x in tokens[1:]]) 
+    word_embedding = np.array(word_embedding)
+    rela_embedding = np.array(rela_embedding)
+
+    model = HR_BiLSTM(dropout, hidden_size, word_embedding, rela_embedding).cuda()
+    optimizer = th.optim.Adadelta(filter(lambda p: p.requires_grad, model.parameters()), lr=learning_rate)
+
+    # 1 batch, batch_size = 4
+    # question : maxlen = 6
+    # pos_relas: maxlen = 3
+    # pos_words: maxlen = 5
+    question_data = [[[1,2,3,0,0,0],[1,2,3,4,0,0],[1,2,3,4,5,6],[1,2,0,0,0,0]]]
+    pos_relas = [[[1,2,3],[1,2,0],[1,2,3],[1,0,0]]]
+    pos_words = [[[1,2,0,0,0],[1,2,3,4,5],[1,0,0,0,0],[1,2,3,0,0]]]
+    q_len = [[3,4,6,2]]
+    pos_r_len = [[3,2,3,1]]
+    pos_w_len = [[2,5,1,3]]
+    for batch_count, question in enumerate(question_data, 1):
+        q_length = Variable(th.LongTensor(q_len[batch_count-1])).cuda()
+        pos_r_length = Variable(th.LongTensor(pos_r_len[batch_count-1])).cuda()
+        pos_w_length = Variable(th.LongTensor(pos_w_len[batch_count-1])).cuda()
+
+        q = Variable(th.LongTensor(question)).cuda()
+        p_relas = Variable(th.LongTensor(pos_relas[batch_count-1])).cuda()
+        p_words = Variable(th.LongTensor(pos_words[batch_count-1])).cuda()
+            
+        optimizer.zero_grad()
+        all_pos_score = model(q, p_relas, p_words)
+        #all_pos_score = model(q, p_relas, p_words, q_length, pos_r_length, pos_w_length)
+        sys.exit()